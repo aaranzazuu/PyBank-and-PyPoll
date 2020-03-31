d31fdc20658901879b88c74982e88d7348b272fc
Aranzazu Ballesteros <aaranzazuu@gmail.com>
2020-03-30 18:11:19 -0700

    sum_changes
    right formula, wrong results

--- PyBank/Analysis/python_homework.py
+++ PyBank/Analysis/python_homework.py
@@ -3,28 +3,28 @@ file = '../Resources/03-Python_HW_Instructions_PyBank_Resources_budget_data.csv'
 with open(file,'r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     csv_header = next(csvreader)
+    first_row = next(csvreader)
 
-    count = 0 
-    net_profit = 0
+    count = 1
+    net_profit = int(first_row[1])
+    previous_value = int(first_row[1])
+    sum_changes = 0
 
 # Total number of months included in the dataset
     for row in csvreader:
         count = count + 1
         net_profit = net_profit + int(row[1])
+        changes = int(row[1])-int(previous_value)
+        sum_changes = sum_changes + changes
+        previous_value = int(row[1])
     
-    def average (profits):
-        avg = count/net_profit
-        return avg
-    net_list=row[1]
+    net_changes = sum_changes/count
+
     print("Financial Analysis")
     print("--------------------")
     print("Total Months: " + str(count))
     print("Total: $" + str(net_profit))
-    print("Average Change: $" + str(avg(net_list)))
+    print(str(changes))
+    print(str(net_changes))
     print("Greatest Increase in Profits:")
     print("Greatest Decrease in Profits")
-
-        
-
-
-
