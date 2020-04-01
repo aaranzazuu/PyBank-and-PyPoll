264d9c7a7a437996a13ebc650422e8cfe6226fc5
Aranzazu Ballesteros <aaranzazuu@gmail.com>
2020-03-31 18:31:31 -0700

    greatest increase and decrease
    correct values, incorrect months

--- PyBank/Analysis/python_homework.py
+++ PyBank/Analysis/python_homework.py
@@ -8,22 +8,38 @@ with open(file,'r') as csvfile:
     count = 1
     net_profit = int(first_row[1])
     previous_value = int(first_row[1])
+    previous_month = first_row[0]
     sum_changes = 0
+    greatest_value = int(first_row[1])
+    greatest_value_month = first_row[0]
+    lowest_value = int(first_row[1])
+    lowest_value_month = first_row[0]
+    list_changes = []
 
 # Total number of months included in the dataset
     for row in csvreader:
         count = count + 1
         net_profit = net_profit + int(row[1])
         changes = int(row[1])-int(previous_value)
+        list_changes.append(changes)
         sum_changes = sum_changes + changes
         previous_value = int(row[1])
-    
+        if greatest_value > int(row[1]):
+            greatest_value_month = greatest_value_month
+        else: 
+            greatest_value_month = row[0]
+        if lowest_value > int(row[1]):
+            lowest_value_month = lowest_value_month
+        else: 
+            lowest_value_month = row[0]
     average_changes = sum_changes/(count-1)
+    greatest_increase = max(list_changes)
+    greatest_decrease = min(list_changes)
     
     print("Financial Analysis")
     print("--------------------")
     print("Total Months: " + str(count))
     print("Total: $" + str(net_profit))
     print("Average Change: $ "+ str(average_changes))
-    print("Greatest Increase in Profits:")
-    print("Greatest Decrease in Profits")
+    print("Greatest Increase in Profits:" + greatest_value_month + "($" + str(greatest_increase)+ ")")
+    print("Greatest Decrease in Profits:" + lowest_value_month + "($" + str(greatest_decrease)+ ")")
