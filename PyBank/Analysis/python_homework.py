908f9ea694574c76bdefbcbe26bd30b7499b1fa0
Aranzazu Ballesteros <aaranzazuu@gmail.com>
2020-03-31 21:39:48 -0700

    export txt
    works

--- PyBank/Analysis/python_homework.py
+++ PyBank/Analysis/python_homework.py
@@ -16,12 +16,16 @@ with open(file,'r') as csvfile:
     lowest_value_month = first_row[0]
     list_changes = []
 
-# Total number of months included in the dataset
+
     for row in csvreader:
+        #Total number of months in the dataset
         count = count + 1
+        #Net total amount of Profit/Losses over the entire period
         net_profit = net_profit + int(row[1])
+        #Calculate changes for the averages
         changes = int(row[1])-int(previous_value)
         list_changes.append(changes)
+        #Calculate the month where the greates changes happened
         if int(greatest_value) > int(row[1]):
             greatest_value = greatest_value
             greatest_value_month = greatest_value_month
@@ -34,12 +38,19 @@ with open(file,'r') as csvfile:
         else: 
             lowest_value = int(row[1])
             lowest_value_month = row[0]
-        sum_changes = sum_changes + changes
+        #Reset variable previous_value
         previous_value = int(row[1])
-    average_changes = sum_changes/(count-1)
-    greatest_increase = max(list_changes)
-    greatest_decrease = min(list_changes)
+        #Calculations for average
+        sum_changes = sum_changes + changes
     
+    #Calculate average changes
+    average_changes = round(sum_changes/(count-1),2)
+
+    #Calculate greates increase in profits and greatest decrease in losses
+    greatest_increase = round(max(list_changes),2)
+    greatest_decrease = round(min(list_changes),2)
+    
+    #Print Analysis to Terminal
     print("Financial Analysis")
     print("--------------------")
     print("Total Months: " + str(count))
@@ -47,3 +58,16 @@ with open(file,'r') as csvfile:
     print("Average Change: $ "+ str(average_changes))
     print("Greatest Increase in Profits:" + greatest_value_month + "($" + str(greatest_increase)+ ")")
     print("Greatest Decrease in Profits:" + lowest_value_month + "($" + str(greatest_decrease)+ ")")
+    
+#Export
+
+output_path = '../Analysis/Financial_Analysis.txt'
+with open(output_path, 'w') as csvfile:
+    csvwriter = csv.writer(csvfile, delimiter=',')
+    csvwriter.writerow("Financial Analysis")
+    csvwriter.writerow("----------------")
+    csvwriter.writerow("Total Months: " + str(count)) 
+    csvwriter.writerow("Total: $: " + str(net_profit))
+    csvwriter.writerow("Average Change: $ "+ str(average_changes))
+    csvwriter.writerow("Greatest Increase in Profits:" + greatest_value_month + "($" + str(greatest_increase)+ ")")
+    csvwriter.writerow("Greatest Decrease in Profits:" + lowest_value_month + "($" + str(greatest_decrease)+ ")")
